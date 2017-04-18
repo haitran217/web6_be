9cd42c62ef885b5cd11c3123066f93bb28d09086
haitran <giacatngoalong123@gmail.com>
2017-04-19 02:18:53 +0700

    cái delete hoi sida a

--- app.js
+++ app.js
@@ -1,46 +1,90 @@
-console.log("hello world");
+console.log('Hello nodemon');
 
 const fs = require('fs');
-
-//dung cac thu vien express
-
+//dung cai thu vien express
 const express = require('express');
+const bodyParser = require('body-parser');
 
+const imageController = require(__dirname + '/modules/images/imageController');
 var app = express();
 
-//set public file public
-// app.use(ur)
+//set public folder public
+//app.use(urlencoded)
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json({
+  extended: true
+}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get('/',function(reg,res){
+
+app.get('/', (req, res) => {
   res.send('./public/index.html');
 })
 
-app.get('/image/add',(req,res) => {
+
+app.post('/image', (req, res) => {
+  //doc du lieu
+  var imageInfoCollection = imageController.fetchImageCollection();
+
+
   //khai bao object
-  var imageinfo = {
-    name : req.query.name,
-    imagelink : req.query.imagelink,
-    description : req.query.description
+  var imageInfo = {
+    name: req.body.name,
+    imageLink: req.body.imageLink,
+    description: req.body.description
   }
+  //push date vafo collection
+  imageInfoCollection.push(imageInfo);
   //luu lai vao file
-  fs.writeFileSync('imageData.json',JSON.stringify(imageinfo));
-  //baso thanh cong
+  imageController.saveImageCollection(imageInfoCollection);
+  //bao thanh cong
   res.send('Success');
 })
-//
 
-app.get('/image/get',(rep,res) =>{
-  var img = fs.readFileSync('imageData.json','utf-8');
+app.get('/image', (req, res) => {
+  var imageInfoCollection = imageController.fetchImageCollection();
 
-  var imgJson = JSON.parse(img);
-  var listImg = '';
-    listImg += '<h1>' + imgJson.name +'</h1>'+
-               '<h2>' + imgJson.description +'</h2>'+
-               '<img src = "' + imgJson.imagelink + '">';
-  res.send(listImg);
+
+  var htmlstring = '';
+
+  imageInfoCollection.forEach((data) => {
+    htmlstring += `<div>${data.name}</div><img
+    src="${data.imageLink}"><div>${data.description}</div>`;
+  })
+  res.send(htmlstring);
 })
 
-app.listen(6969,function(reg,res){
+app.post('/image/update', (req, res) => {
+  var imageInfoCollection = imageController.fetchImageCollection();
+  var nameOld = req.body.nameOld;
+  var nameNew = req.body.nameNew;
+  imageInfoCollection.forEach((data) => {
+    if (data.name == nameOld) {
+      data.name = nameNew;
+    }
+  })
+  imageController.saveImageCollection(imageInfoCollection);
+  res.redirect('/image');
+})
+
+app.post('/image/delete', (req, res) => {
+  var imageInfoCollection = imageController.fetchImageCollection();
+  var nameDelete = req.body.nameDelete;
+  console.log(nameDelete);
+  imageInfoCollection.forEach((data) => {
+    if (data.name == nameDelete) {
+      delete data.name;
+      delete data.imageLink;
+      delete data.description;
+    }
+  })
+  imageController.saveImageCollection(imageInfoCollection);
+  res.redirect('/image');
+})
+
+//mo 1 cai port de chay local
+app.listen(6969, (req, res) => {
   console.log('app listen on 6969');
 })
